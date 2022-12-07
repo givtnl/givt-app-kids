57a43a56745c1f245aeffcdd53e172683b2fd521
Pavlo Pavelko <p.pavelko.job@gmail.com>
2022-12-07 20:12:36 +0200

    Brand new flutter app for kids is created. The app ids updated to net.givtapp.kids

---
+++ android/app/src/main/kotlin/com/example/givt_app_kids/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.givt_app_kids
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Givt App Kids',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Givt App Kids"),
+      ),
+      body: Center(
+        child: Text(
+          "Hello Kids!",
+          style: Theme.of(context).textTheme.headline2,
+        ),
+      ),
+    );
+  }
+}
