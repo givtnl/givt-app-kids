3f3e8bbbc44454a393a66208f5c768f176c6bbd1
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-02-14 08:32:12 +0100

    bigger eye icon + reversed svg

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -223,8 +223,8 @@ class _LoginScreenState extends State<LoginScreen> {
                                 },
                                 child: SvgPicture.asset(
                                   _isPasswordVisible
-                                      ? "assets/images/password_show.svg"
-                                      : "assets/images/password_hide.svg",
+                                      ? "assets/images/password_hide.svg"
+                                      : "assets/images/password_show.svg",
                                   width: 20,
                                   height: 20,
                                 ),
