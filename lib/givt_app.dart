143b3ce6adcf770509368e87535409d72b57682a
Daniela Damaschin <whateverellie@gmail.com>
2023-06-09 14:17:48 +0200

    delete import

--- lib/givt_app.dart
+++ lib/givt_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:givt_app_kids/features/auth/cubit/auth_cubit.dart';
-import 'package:givt_app_kids/features/auth/screens/login_screen.dart';
 import 'package:givt_app_kids/features/wallet/wallet_screen.dart';
 import 'package:givt_app_kids/features/auth/screens/login_screen.dart'
     as login_bloc;
