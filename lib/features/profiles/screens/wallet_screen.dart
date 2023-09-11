ed64ccb3798a88219bac037dec3908b0c2b750e0
Daniela Damaschin <whateverellie@gmail.com>
2023-09-11 16:24:03 +0200

    KIDS 1265 Refresh profile on AppLifecycleState Change

--- lib/features/profiles/screens/wallet_screen.dart
+++ lib/features/profiles/screens/wallet_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:givt_app_kids/features/auth/cubit/auth_cubit.dart';
@@ -20,23 +22,43 @@ class WalletScreenCubit extends StatefulWidget {
   State<WalletScreenCubit> createState() => _WalletScreenCubitState();
 }
 
-class _WalletScreenCubitState extends State<WalletScreenCubit> {
+class _WalletScreenCubitState extends State<WalletScreenCubit>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    log('state = $state');
+    refresh();
+  }
+
+  Future<void> refresh() async {
+    final parentGuid =
+        (context.read<AuthCubit>().state as LoggedInState).session.userGUID;
+    await context.read<ProfilesCubit>().fetchProfiles(parentGuid);
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return BlocBuilder<ProfilesCubit, ProfilesState>(builder: (context, state) {
       final isPending = state.activeProfile.wallet.pending > 0.0;
       final isGiveButtonActive = state.activeProfile.wallet.balance > 0;
+      final isLoading = state is ProfilesLoadingState;
 
       var countdownAmount = 0.0;
       if (state is ProfilesCountdownState) {
         countdownAmount = state.amount;
-      }
-
-      Future<void> refresh() async {
-        final parentGuid =
-            (context.read<AuthCubit>().state as LoggedInState).session.userGUID;
-        await context.read<ProfilesCubit>().fetchProfiles(parentGuid);
       }
 
       return WalletFrame(
@@ -50,10 +72,13 @@ class _WalletScreenCubitState extends State<WalletScreenCubit> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Heading2(text: state.activeProfile.firstName),
-                  WalletWidget(
-                    balance: state.activeProfile.wallet.balance,
-                    countdownAmount: countdownAmount,
-                  ),
+                  if (isLoading)
+                    const Center(child: CircularProgressIndicator())
+                  else
+                    WalletWidget(
+                      balance: state.activeProfile.wallet.balance,
+                      countdownAmount: countdownAmount,
+                    ),
                   SizedBox(height: size.height * 0.01),
                   QrGiveButton(isActive: isGiveButtonActive),
                   if (isPending) SizedBox(height: size.height * 0.03),
