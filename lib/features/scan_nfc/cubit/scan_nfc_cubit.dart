fc8072348ede5ed4ba0f8935bf124a27cb88d248
Daniela Damaschin <whateverellie@gmail.com>
2023-11-10 13:28:18 +0100

    comment out slider screen route logic

--- lib/features/scan_nfc/cubit/scan_nfc_cubit.dart
+++ lib/features/scan_nfc/cubit/scan_nfc_cubit.dart
@@ -32,50 +32,56 @@ class ScanNfcCubit extends Cubit<ScanNfcState> {
     emit(state.copyWith(
       scanNFCStatus: ScanNFCStatus.scanning,
     ));
+
     String mediumId = '';
     String readData = '';
-    try {
-      NfcManager.instance.startSession(
-          alertMessage: 'Tap your coin to the top\nof the iPhone',
-          onDiscovered: (NfcTag tag) async {
-            log('coin discovered: ${tag.data}');
-            var ndef = Ndef.from(tag);
-            if (ndef != null && ndef.cachedMessage != null) {
-              if (ndef.cachedMessage!.records.isNotEmpty &&
-                  ndef.cachedMessage!.records.first.typeNameFormat ==
-                      NdefTypeNameFormat.nfcWellknown) {
-                final wellKnownRecord = ndef.cachedMessage!.records.first;
-                if (wellKnownRecord.payload.first == 0x02) {
-                  final languageCodeAndContentBytes =
-                      wellKnownRecord.payload.skip(1).toList();
-                  final languageCodeAndContentText =
-                      utf8.decode(languageCodeAndContentBytes);
-                  final payload = languageCodeAndContentText.substring(2);
-                  log('coin payload: $payload');
-                  Uri uri = Uri.parse(payload);
-                  mediumId = uri.queryParameters['code'] ?? mediumId;
-                  readData = payload;
-                } else {
-                  final decoded = utf8.decode(wellKnownRecord.payload);
-                  log('coin decoded: $decoded');
-                  Uri uri = Uri.parse(decoded);
-                  mediumId = uri.queryParameters['code'] ?? mediumId;
-                  readData = decoded;
-                }
-                emit(state.copyWith(
-                    mediumId: mediumId,
-                    readData: readData,
-                    scanNFCStatus: ScanNFCStatus.scanned,
-                    coinAnimationStatus: CoinAnimationStatus.stopped));
-                NfcManager.instance.stopSession();
-              }
-            }
-          });
-    } catch (e) {
-      emit(state.copyWith(
-          scanNFCStatus: ScanNFCStatus.error,
-          coinAnimationStatus: CoinAnimationStatus.stopped));
-      NfcManager.instance.stopSession();
-    }
+    emit(state.copyWith(
+        mediumId: mediumId,
+        readData: readData,
+        scanNFCStatus: ScanNFCStatus.scanned,
+        coinAnimationStatus: CoinAnimationStatus.stopped));
+    // try {
+    //     NfcManager.instance.startSession(
+    //         alertMessage: 'Tap your coin to the top\nof the iPhone',
+    //         onDiscovered: (NfcTag tag) async {
+    //           log('coin discovered: ${tag.data}');
+    //           var ndef = Ndef.from(tag);
+    //           if (ndef != null && ndef.cachedMessage != null) {
+    //             if (ndef.cachedMessage!.records.isNotEmpty &&
+    //                 ndef.cachedMessage!.records.first.typeNameFormat ==
+    //                     NdefTypeNameFormat.nfcWellknown) {
+    //               final wellKnownRecord = ndef.cachedMessage!.records.first;
+    //               if (wellKnownRecord.payload.first == 0x02) {
+    //                 final languageCodeAndContentBytes =
+    //                     wellKnownRecord.payload.skip(1).toList();
+    //                 final languageCodeAndContentText =
+    //                     utf8.decode(languageCodeAndContentBytes);
+    //                 final payload = languageCodeAndContentText.substring(2);
+    //                 log('coin payload: $payload');
+    //                 Uri uri = Uri.parse(payload);
+    //                 mediumId = uri.queryParameters['code'] ?? mediumId;
+    //                 readData = payload;
+    //               } else {
+    //                 final decoded = utf8.decode(wellKnownRecord.payload);
+    //                 log('coin decoded: $decoded');
+    //                 Uri uri = Uri.parse(decoded);
+    //                 mediumId = uri.queryParameters['code'] ?? mediumId;
+    //                 readData = decoded;
+    //               }
+    //               emit(state.copyWith(
+    //                   mediumId: mediumId,
+    //                   readData: readData,
+    //                   scanNFCStatus: ScanNFCStatus.scanned,
+    //                   coinAnimationStatus: CoinAnimationStatus.stopped));
+    //               NfcManager.instance.stopSession();
+    //             }
+    //           }
+    //         });
+    //   } catch (e) {
+    //     emit(state.copyWith(
+    //         scanNFCStatus: ScanNFCStatus.error,
+    //         coinAnimationStatus: CoinAnimationStatus.stopped));
+    //     NfcManager.instance.stopSession();
+    //   }
   }
 }
