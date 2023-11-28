993024e519db4504bae0f4ceec7174b18318670e
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-11-28 09:23:25 +0100

    Add imports and update UI in OrganisationsScreen

--- lib/features/recommendation/organisations/screens/organisations_screen.dart
+++ lib/features/recommendation/organisations/screens/organisations_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
@@ -5,14 +6,36 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:givt_app_kids/features/recommendation/organisations/cubit/organisations_cubit.dart';
 import 'package:givt_app_kids/features/recommendation/organisations/widgets/organisation_item.dart';
 import 'package:givt_app_kids/features/recommendation/widgets/recommendation_givy_bubble.dart';
+import 'package:givt_app_kids/helpers/analytics_helper.dart';
+import 'package:givt_app_kids/helpers/snack_bar_helper.dart';
 import 'package:givt_app_kids/shared/widgets/givt_back_button.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class OrganisationsScreen extends StatelessWidget {
   const OrganisationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<OrganisationsCubit, OrganisationsState>(
+    return BlocConsumer<OrganisationsCubit, OrganisationsState>(
+      listener: (context, state) {
+        if (state is OrganisationsExternalErrorState) {
+          log(state.errorMessage);
+          SnackBarHelper.showMessage(
+            context,
+            text:
+                'Cannot recommend organisations. Please try again later. ${state.errorMessage}',
+            isError: true,
+          );
+        } else if (state is OrganisationsFetchedState) {
+          AnalyticsHelper.logEvent(
+            eventName: AmplitudeEvent.charitiesShown,
+            eventProperties: {
+              AnalyticsHelper.recommendedCharitiesKey:
+                  state.organisations.map((e) => e.name).toList().toString(),
+            },
+          );
+        }
+      },
       builder: (context, state) {
         return Scaffold(
           body: Container(
@@ -50,6 +73,9 @@ class OrganisationsScreen extends StatelessWidget {
                       ),
                     ),
                   ),
+                  // if (state is OrganisationsFetchingState)
+                  //   LoadingAnimationWidget.waveDots(
+                  //       color: const Color(0xFF54A1EE), size: 50),
                   SliverPadding(
                     padding: const EdgeInsets.symmetric(
                       horizontal: 24,
@@ -72,80 +98,5 @@ class OrganisationsScreen extends StatelessWidget {
         );
       },
     );
-
-    // @override
-    // Widget build(BuildContext context) {
-    //   final size = MediaQuery.of(context).size;
-    //   return BlocConsumer<OrganisationsCubit, OrganisationsState>(
-    //     listener: (context, state) {
-    //       if (state is OrganisationsExternalErrorState) {
-    //         log(state.errorMessage);
-    //         SnackBarHelper.showMessage(
-    //           context,
-    //           text:
-    //               'Cannot recommend organisations. Please try again later. ${state.errorMessage}',
-    //           isError: true,
-    //         );
-    //       } else if (state is OrganisationsFetchedState) {
-    //         AnalyticsHelper.logEvent(
-    //           eventName: AmplitudeEvent.charitiesShown,
-    //           eventProperties: {
-    //             AnalyticsHelper.recommendedCharitiesKey:
-    //                 state.organisations.map((e) => e.name).toList().toString(),
-    //           },
-    //         );
-    //       }
-    //     },
-    //           child: SafeArea(
-    //             child: Column(
-    //               crossAxisAlignment: CrossAxisAlignment.center,
-    //               mainAxisSize: MainAxisSize.min,
-    //               children: [
-    //                 RecommendationGivyBubble(
-    //                   text: state is OrganisationsFetchingState
-    //                       ? 'Loading...'
-    //                       : state.organisations.isEmpty
-    //                           ? 'Oops, something went wrong...'
-    //                           : 'These charities fit your interests!',
-    //                 ),
-    //                 if (state is OrganisationsFetchingState)
-    //                   Padding(
-    //                     padding: EdgeInsets.only(top: size.height * 0.25),
-    //                     child: LoadingAnimationWidget.waveDots(
-    //                         color: const Color(0xFF54A1EE),
-    //                         size: size.width * 0.2),
-    //                   ),
-    //                 Expanded(
-    //                   child: SingleChildScrollView(
-    //                     child: Padding(
-    //                       padding: const EdgeInsets.symmetric(
-    //                           horizontal: 24, vertical: 10),
-    //                       child: Column(
-    //                         mainAxisSize: MainAxisSize.min,
-    //                         children: state.organisations
-    //                             .map(
-    //                               (organisation) => Padding(
-    //                                 padding:
-    //                                     const EdgeInsets.symmetric(vertical: 10),
-    //                                 child: OrganisationItem(
-    //                                   width: double.maxFinite,
-    //                                   height: size.height * .4,
-    //                                   organisation: organisation,
-    //                                 ),
-    //                               ),
-    //                             )
-    //                             .toList(),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ),
-    //                 const SizedBox(height: 20),
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //       );
-    //     },
-    //   );
   }
 }
