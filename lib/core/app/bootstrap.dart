d5e6a7700e1bc602be256e0f199862128e171c75
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-03-02 15:42:43 +0100

    Add Firebase configuration

--- lib/core/app/bootstrap.dart
+++ lib/core/app/bootstrap.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'dart:developer';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:givt_app_kids/core/app/givt_app.dart';
 import 'package:givt_app_kids/core/logging/logging.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:givt_app_kids/core/app/firebase_options.dart' as firebase_prod_options;
+import 'package:givt_app_kids/core/app/firebase_options_dev.dart' as firebase_dev_options;
 
 import 'package:givt_app_kids/core/injection/injection.dart' as get_it;
 
@@ -42,4 +46,19 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
       );
     },
   );
+}
+
+/// Returns the firebase options
+/// and the current platform
+/// based on the current build flavor
+Future<(String, FirebaseOptions)> get _firebaseOptions async {
+  final info = await PackageInfo.fromPlatform();
+  final isDebug = info.packageName.contains('test');
+
+  final name = isDebug ? 'givt4kids-test' : 'givt4kids';
+  final options = isDebug
+      ? firebase_dev_options.DefaultFirebaseOptions.currentPlatform
+      : firebase_prod_options.DefaultFirebaseOptions.currentPlatform;
+
+  return (name, options);
 }
