3cdedc158b96f4bc85247c4f992bb5b11d30f755
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-03-25 10:48:45 +0100

    unneccessary

--- lib/features/qr_scanner/cubit/camera_cubit.dart
+++ lib/features/qr_scanner/cubit/camera_cubit.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:givt_app_kids/core/injection/injection.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 part 'camera_state.dart';
 
 class CameraCubit extends Cubit<CameraState> {
@@ -19,27 +15,11 @@ class CameraCubit extends Cubit<CameraState> {
     //delay is from design
     Future.delayed(permisionDialogDelay);
 
-    // iOS returns denied status in case
-    // the user was not asked for permissions yet or
-    // it was permanently denied
-    if (status.isDenied && Platform.isIOS) {
-      final iOSCameraPermissionAsked =
-          getIt<SharedPreferences>().getBool(cameraPermissionAskedKey);
-      // if it is the first time asking for permission on iOS, remember it
-      if (iOSCameraPermissionAsked == null) {
-        getIt<SharedPreferences>().setBool(cameraPermissionAskedKey, true);
-        emit(const CameraPermissionRequest());
-        return;
-      }
-      emit(const CameraPermissionPermanentlyDeclined());
-      return;
-    }
-
     if (status.isDenied) {
       emit(const CameraPermissionRequest());
       return;
     }
-    // android only status, once a user denied permission twice
+
     if (status.isPermanentlyDenied) {
       emit(const CameraPermissionPermanentlyDeclined());
       return;
