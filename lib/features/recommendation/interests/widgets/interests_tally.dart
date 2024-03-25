b1ec027d8e277eb06e0fe0a9a8ed64b4eab42290
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-03-25 15:19:37 +0100

    New interest counter

--- lib/features/recommendation/interests/widgets/interests_tally.dart
+++ lib/features/recommendation/interests/widgets/interests_tally.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:givt_app_kids/features/recommendation/interests/cubit/interests_cubit.dart';
+
 import 'package:givt_app_kids/helpers/app_theme.dart';
 
 class InterestsTally extends StatelessWidget {
@@ -13,20 +13,36 @@ class InterestsTally extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Row(
+      children: List<Widget>.generate(
+        InterestsState.maxInterests,
+        // index + 1 because we start 1 while the index starts at 0
+        (index) => _buildTallyIcon(index + 1, context),
+      ),
+    );
+  }
+
+  Widget _buildTallyIcon(int index, BuildContext context) {
     return Container(
-      width: 40,
-      height: 40,
+      width: 24,
+      height: 24,
+      padding: const EdgeInsets.only(bottom: 0.5),
+      margin: const EdgeInsets.only(right: 4),
       alignment: Alignment.center,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: AppTheme.givt4KidsBlue,
+        borderRadius: BorderRadius.circular(12),
+        color: tally >= index
+            ? AppTheme.primary70
+            : Theme.of(context).colorScheme.surfaceVariant,
       ),
       child: Text(
-        '$tally/${InterestsState.maxInterests}',
-        style: Theme.of(context).textTheme.titleSmall?.copyWith(
-              color: AppTheme.interestsTallyText,
-              fontFamily: 'RadioCanada',
-            ),
+        index.toStringAsFixed(0),
+        style: tally >= index
+            ? Theme.of(context).textTheme.bodySmall
+            : Theme.of(context)
+                .textTheme
+                .bodySmall
+                ?.copyWith(color: Theme.of(context).colorScheme.outline),
       ),
     );
   }
