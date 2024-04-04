f0479763168b386e09f183b27a6e56d61bf1034c
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-04-04 21:18:35 +0200

    Update  error handling with crashlytics

--- lib/core/app/bootstrap.dart
+++ lib/core/app/bootstrap.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -77,7 +78,13 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   );
 
   FlutterError.onError = (details) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
     log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
   };
 
   await runZonedGuarded(
--- lib/features/profiles/widgets/give_bottomsheet.dart
+++ lib/features/profiles/widgets/give_bottomsheet.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app_kids/core/app/pages.dart';
