06cf057ff952254f8e9581566dec5a249fc9077b
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-04-12 10:44:06 +0200

    do not use index

--- lib/features/home_screen/cubit/navigation_cubit.dart
+++ lib/features/home_screen/cubit/navigation_cubit.dart
@@ -5,22 +5,9 @@ part 'navigation_state.dart';
 
 class NavigationCubit extends Cubit<NavigationState> {
   NavigationCubit() : super(const NavigationState());
-  void changePage(int index) {
-    switch (index) {
-      case 0:
-        emit(state.copyWith(
-            activeDestination: NavigationDestinationData.values[0]));
-        break;
-      case 1:
-        emit(state.copyWith(
-            activeDestination: NavigationDestinationData.values[1]));
-        break;
-      case 2:
-        emit(state.copyWith(
-            activeDestination: NavigationDestinationData.values[2]));
-        break;
-      default:
-        emit(state.copyWith(activeDestination: NavigationDestinationData.home));
-    }
+  void changePage(NavigationDestinationData page) {
+    emit(
+      state.copyWith(activeDestination: page),
+    );
   }
 }
