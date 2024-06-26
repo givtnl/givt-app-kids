d93639a4b57332153cc98de72da938b78b7aca4e
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-06-26 11:35:00 +0200

    remove from homescreen, only popover profilescreen

--- lib/features/home_screen/home_screen.dart
+++ lib/features/home_screen/home_screen.dart
@@ -12,69 +12,52 @@ import 'package:givt_app_kids/features/profiles/screens/profile_screen.dart';
 import 'package:givt_app_kids/features/home_screen/widgets/custom_navigation_bar.dart';
 import 'package:givt_app_kids/features/home_screen/widgets/home_screen_app_bar.dart';
 import 'package:givt_app_kids/helpers/analytics_helper.dart';
-import 'package:givt_app_kids/helpers/school_event_helper.dart';
-import 'package:givt_app_kids/shared/widgets/download_givt_app_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: const HomeScreenAppBar(),
-          bottomNavigationBar: CustomNavigationBar(
-            index:
-                context.watch<NavigationCubit>().state.activeDestination.index,
-            onDestinationSelected: (int index) {
-              SystemSound.play(SystemSoundType.click);
-              HapticFeedback.selectionClick();
+    return Scaffold(
+      appBar: const HomeScreenAppBar(),
+      bottomNavigationBar: CustomNavigationBar(
+        index: context.watch<NavigationCubit>().state.activeDestination.index,
+        onDestinationSelected: (int index) {
+          SystemSound.play(SystemSoundType.click);
+          HapticFeedback.selectionClick();
 
-              context
-                  .read<NavigationCubit>()
-                  .changePage(NavigationDestinationData.values[index]);
+          context
+              .read<NavigationCubit>()
+              .changePage(NavigationDestinationData.values[index]);
 
-              AnalyticsHelper.logEvent(
-                  eventName: AmplitudeEvent.navigationBarPressed,
-                  eventProperties: {
-                    'destination': NavigationDestinationData.values[index].name
-                  });
-            },
-          ),
-          body: BlocBuilder<NavigationCubit, NavigationState>(
-            builder: (context, state) {
-              return AnimatedSwitcher(
+          AnalyticsHelper.logEvent(
+              eventName: AmplitudeEvent.navigationBarPressed,
+              eventProperties: {
+                'destination': NavigationDestinationData.values[index].name
+              });
+        },
+      ),
+      body: BlocBuilder<NavigationCubit, NavigationState>(
+        builder: (context, state) {
+          return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: AnimatedSwitcher(
                 duration: const Duration(milliseconds: 300),
-                child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 300),
-                    switchInCurve: Curves.easeInOutQuart,
-                    switchOutCurve: Curves.easeInOutQuart,
-                    transitionBuilder: (child, animation) {
-                      return SlideTransition(
-                        position: Tween<Offset>(
-                          begin: const Offset(0, 0.3),
-                          end: Offset.zero,
-                        ).animate(animation),
-                        child: child,
-                      );
-                    },
-                    child: getPage(state.activeDestination, context)),
-              );
-            },
-          ),
-        ),
-        Visibility(
-            visible: SchoolEventHelper.isSchoolEventUser(context) &&
-                context
-                        .read<ProfilesCubit>()
-                        .state
-                        .activeProfile
-                        .wallet
-                        .balance ==
-                    0,
-            child: const DownloadGivtAppDialog()),
-      ],
+                switchInCurve: Curves.easeInOutQuart,
+                switchOutCurve: Curves.easeInOutQuart,
+                transitionBuilder: (child, animation) {
+                  return SlideTransition(
+                    position: Tween<Offset>(
+                      begin: const Offset(0, 0.3),
+                      end: Offset.zero,
+                    ).animate(animation),
+                    child: child,
+                  );
+                },
+                child: getPage(state.activeDestination, context)),
+          );
+        },
+      ),
     );
   }
 
